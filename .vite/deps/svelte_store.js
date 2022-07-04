9e8bc6dc686ae7349fa4a598e4655d75228a2b26
Ozurexus <pinyaguinm@mail.ru>
2022-07-04 19:45:32 +0300

    First commit to sveltekit

---
+++ .vite/deps/svelte_store.js
@@ -0,0 +1,15 @@
+import {
+  derived,
+  readable,
+  writable
+} from "./chunk-RLI7XDGH.js";
+import {
+  get_store_value
+} from "./chunk-PG6ZNUGI.js";
+export {
+  derived,
+  get_store_value as get,
+  readable,
+  writable
+};
+//# sourceMappingURL=svelte_store.js.map
---
+++ src/routes/index.svelte
@@ -0,0 +1,38 @@
+<script lang="ts">
+</script>
+
+<body>
+    <h1>Hi!</h1>
+    <p>My name is Maxim Pinyaguin and welcome to my very own site</p>
+    <nav>
+        Navigation:
+        <a href="./about">About me</a>
+        <a href="./projects">Projects</a>
+        <a href="./contact">Contact me</a>
+        <a href="./dogAPI">Click to see dogs</a>
+    </nav>
+</body>
+
+<style>
+    body {
+        background: url(/src/images/nasa.jpg) no-repeat center center fixed;
+        background-size: cover;
+        color: whitesmoke;
+        font-size: x-large;
+    }
+
+    a {
+        margin-right: 10px;
+        color: aqua;
+    }
+
+    h1 {
+        font-family: "Segoe UI", Tahoma, Geneva, Verdana, sans-serif;
+    }
+
+    p {
+        font-family: "Gill Sans", "Gill Sans MT", Calibri, "Trebuchet MS",
+            sans-serif;
+        font-style: italic;
+    }
+</style>
